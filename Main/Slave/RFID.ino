9c50dc4af606acc7e4efc981a30ee6fca16aef11
BuiKhoi <khoibui2319@gmail.com>
2018-12-01 12:28:39 +0700

    Fixed reboot

--- Main/Slave/RFID.ino
+++ Main/Slave/RFID.ino
@@ -2,13 +2,16 @@ void GetCardID() {
   strcpy(CardID, "");
   for (byte i = 0; i < 4; i++) {
     int temp = mfrc522.uid.uidByte[i];
-    Serial.print(temp, HEX);
-//    uint8_t str_temp[2];
-//    strcpy(str_temp, "");
-//    str_temp[1] = GetChar(temp % 16);
-//    temp /= 16;
-//    str_temp[0] = GetChar(temp);
-//    strcat(CardID, str_temp);
+    char str_temp[4];
+    strcpy(str_temp, "");
+    str_temp[1] = GetChar(temp % 16);
+    temp /= 16;
+    str_temp[0] = GetChar(temp);
+    CardID[2*i] = str_temp[0];
+    CardID[2*i+1] = str_temp[1];
+  }
+  for (int i=0; i<8; i++) {
+    Serial.print(CardID[i]);
   }
   Serial.println();
 }
